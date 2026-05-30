1b80b8dfa1fb2d2b76c26b6124244d9d917573e9
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-05-30 19:49:08 +0900

    chore: .gitignore 수정

--- server/main.py
+++ server/main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://cpr-frontend.vercel.app/",
+        "https://*.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
